71eb8b66ebc27af08be11d697d5d7c781af87f85
Marlena Tsang <Marlena345@gmail.com>
2022-05-25 16:02:18 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension"
 import App from "./App";
 import rootReducer from "./reducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+const store = createStore(rootReducer, composedEnhancer);
 
 ReactDOM.render(
   <Provider store={store}>
